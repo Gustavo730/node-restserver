4e7fe8f99dde45a206178693c500761e73481f7c
Gustavo Vásquez <gustavovasquez@MacBook-Air-de-Gustavo.local>
2018-07-28 01:35:52 -0400

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,6 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
   urlBD = '//localhost:27017/cafe';
 } else {
-  urlBD = 'mongodb://cafe-user:123456a@ds257981.mlab.com:57981/cafe';
+  urlBD = process.env.MONGO_URI;
 }
 process.env.URL_BD = urlBD;
